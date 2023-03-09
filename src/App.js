58b4ea07595a03c08e88a09ea11288f78ddac717
Kaja Kopczynska <kaja.a.kopczynska@gmail.com>
2023-03-09 13:23:51 +0100

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  }
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
@@ -33,7 +42,12 @@ function App() {
       <Section
         title="Lista zadań"
         body={
-          <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
         }
         extraHeaderContent={
           <Buttons
